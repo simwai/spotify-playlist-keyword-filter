5de3f88afc89e10f7314ac0b30bb10731f42a879
Simon Waiblinger <SimonWaiblinger@live.at>
2023-07-04 17:24:00 +0200

    Deleted nordvpn connection script

--- nordvpn.bat
+++
@@ -1,7 +0,0 @@
-@title NordVPN Connect by simwai
-@echo off
-if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
-cd "C:\Program Files (x86)\NordVPN"
-nordvpn -d 
-nordvpn -c -g "United States"
-exit
