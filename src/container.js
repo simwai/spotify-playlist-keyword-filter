d887c01d4275b4e8e0897628d709b9e5118aaba5
simwai <16225108+simwai@users.noreply.github.com>
2025-06-16 11:41:26 +0200

    feat: Refactor backend to use async container creation and remove build script
    This commit refactors the backend application to use an asynchronous container creation pattern. This allows for more flexible initialization of dependencies, especially when dealing with asynchronous operations like connecting to databases or external services.
    
    Key changes include:
    
    - **Asynchronous Container Creation:** Introduced `createContainer` function in `src/container.js` to asynchronously initialize and return the application container. This replaces the previous synchronous container instantiation.
    - **Dynamic Import for Utils:** Changed the import of `@simwai/utils` to a dynamic import to allow for the use of ES modules.
    - **Updated index.js:** Modified `src/index.js` to call `createContainer` and await the container before starting the application.
    - **Removed Build Script:** Removed the `build` script from `package.json` as it was simply running `npm install` which is unnecessary.
    - **Added TODO:** Added a TODO comment to `src/index.js` to add backend tests.

--- src/container.js
+++ src/container.js
@@ -1,6 +1,4 @@
 const he = require('he')
-const { ConsoleLogger } = require('@simwai/utils')
-
 const { LyricsService } = require('./services/lyrics-service.js')
 const { GeniusApiClient } = require('./clients/genius-api.js')
 const { LyricsExtractor } = require('./utils/lyrics-extractor.js')
@@ -8,29 +6,40 @@ const config = require('./config/index.js')
 const { SpotifyAuthService } = require('./services/spotify-auth-service.js')
 const { SpotifyApiClient } = require('./clients/spotify-api.js')
 const { got } = require('got-cjs')
-
 const httpClient = got
-const logger = new ConsoleLogger({ isTimeEnabled: true })
-const geniusClient = new GeniusApiClient(logger, httpClient, config)
-const lyricsExtractor = new LyricsExtractor(he, logger)
 const LyricsCache = require('./cache/lyrics-cache-model.js')
-const lyricsService = new LyricsService(
-  geniusClient,
-  lyricsExtractor,
-  httpClient,
-  logger,
-  LyricsCache
-)
-const spotifyAuthService = new SpotifyAuthService(logger, config, httpClient)
-const spotifyApiClient = new SpotifyApiClient(httpClient, config)
 
-const container = {
-  lyricsService,
-  logger,
-  config,
-  spotifyAuthService,
-  spotifyApiClient,
-  httpClient,
+// Async function to initialize the container
+async function createContainer() {
+  // Dynamic import for ES module
+  const utils = await import('@simwai/utils')
+  const ConsoleLogger = utils.ConsoleLogger
+
+  const logger = new ConsoleLogger({ isTimeEnabled: true })
+  const geniusClient = new GeniusApiClient(logger, httpClient, config)
+  const lyricsExtractor = new LyricsExtractor(he, logger)
+
+  const lyricsService = new LyricsService(
+    geniusClient,
+    lyricsExtractor,
+    httpClient,
+    logger,
+    LyricsCache
+  )
+
+  const spotifyAuthService = new SpotifyAuthService(logger, config, httpClient)
+  const spotifyApiClient = new SpotifyApiClient(httpClient, config)
+
+  const container = {
+    lyricsService,
+    logger,
+    config,
+    spotifyAuthService,
+    spotifyApiClient,
+    httpClient,
+  }
+
+  return container
 }
 
-module.exports.container = container
+module.exports.createContainer = createContainer
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// TODO Add backend tests
 const express = require('express')
 
 const { setupMiddleware } = require('./middleware/index.js')
@@ -7,25 +8,28 @@ const {
   notFoundHandler,
 } = require('./middleware/error-handler.js')
 const config = require('./config/index.js')
-
-// This import instantiates our container that does some main orchestration
-const { container } = require('./container.js')
+const { createContainer } = require('./container.js')
 
 const _removePortInUrl = (url) => {
   return url.replace(/:\d+/, '')
 }
 
 // Main / Index
-const app = express()
+async function run() {
+  const app = express()
+  const container = await createContainer()
 
-setupMiddleware(app)
-setupRoutes(app, container)
+  setupMiddleware(app)
+  setupRoutes(app, container)
 
-app.use(notFoundHandler)
-app.use(errorHandler)
+  app.use(notFoundHandler)
+  app.use(errorHandler)
 
-app.listen(config.app.port, () => {
-  container.logger.log(
-    `🚀 Server running on ${_removePortInUrl(config.app.frontendUrl)}:${config.app.port}`
-  )
-})
+  app.listen(config.app.port, () => {
+    container.logger.log(
+      `🚀 Server running on ${_removePortInUrl(config.app.frontendUrl)}:${config.app.port}`
+    )
+  })
+}
+
+run()
